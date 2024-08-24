eaad43cca7f1994e2c99d7f53af1f55e8790491f
wanxiang2 <wanxiang@buffalo.edu>
2024-08-24 12:32:47 -0400

    Set up APIs

--- finance-tutorial/middleware.ts
+++ finance-tutorial/middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-    '/',
-])
+    '/'
+]);
 
 export default clerkMiddleware((auth, request) => {
     if (isProtectedRoute(request)) {
